4466f9bbd3aa415f08a5d123a0394434b9beb06d
luskha <lucas.limaacesso@gmail.com>
2024-05-06 21:45:01 -0300

    connect database.js and server.js import

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express"
+import { Sequelize } from "sequelize"
+import { connection } from "./db/database.js"
 const app = express()
 
 const HOST = 'localhost'
 const PORT = 5000
+
+connection()
 
 app.get('/', (req, res) => {
   res.send({
@@ -10,6 +14,15 @@ app.get('/', (req, res) => {
     status: 200
     })
 })
+
+const sequelize = new Sequelize('postgresql://dripstore_db_owner:Ut3dhFCn9wyA@ep-winter-dew-a5niat7j.us-east-2.aws.neon.tech/dripstore_db?sslmode=require')
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.log('Unable to connect to the database:' , error)
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port http://${HOST}:${PORT}`)
